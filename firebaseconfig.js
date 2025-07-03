fc691399b8daeae51af408a3e7deb3acfa2a7bd9
VivekASen <sen14vivek@gmail.com>
2025-07-03 12:36:40 -0400

    Replace with firebase configs

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -10,15 +10,14 @@ import {
   child
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-// Replace with your actual config:
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCMSBZ3SjddXMatw4iwUr8T8aFMY-O-N_I",
+  authDomain: "mtg-prize-wheel.firebaseapp.com",
+  databaseURL: "https://mtg-prize-wheel-default-rtdb.firebaseio.com",
+  projectId: "mtg-prize-wheel",
+  storageBucket: "mtg-prize-wheel.firebasestorage.app",
+  messagingSenderId: "909073049822",
+  appId: "1:909073049822:web:386b014a0be75f972fba4f"
 };
 
 const app = initializeApp(firebaseConfig);
